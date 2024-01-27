3d439da61891489ead0bf1f6c032f5a1ee63b241
Gil <gilvicentevieira@gmail.com>
2024-01-27 22:34:39 +0000

    updated CreateJobForm to use queryClient and create a job on db

--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -16,6 +16,12 @@ import { Form } from '@/components/ui/form';
 import { CustomFormField, CustomFormSelect } from './FormComponents';
 import { log } from 'console';
 
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createJobAction } from '@/utils/actions';
+import { useToast } from '@/components/ui/use-toast';
+import { useRouter } from 'next/navigation';
+
+
 const CreateJobForm = () => {
     const form = useForm<CreateAndEditJobType>({
         resolver:zodResolver(createAndEditJobSchema),
@@ -28,8 +34,26 @@ const CreateJobForm = () => {
         }
     })
 
+    const queryClient = useQueryClient()
+    const {toast} = useToast()
+    const router = useRouter()
+    const {mutate, isPending} = useMutation({
+        mutationFn: (values:CreateAndEditJobType)=>createJobAction(values),
+        onSuccess:(data)=>{
+            if(!data){
+                toast({description: 'there was an error'})
+                return;
+            }
+            toast({description:'job created'})
+            queryClient.invalidateQueries({queryKey:['jobs']})
+            queryClient.invalidateQueries({queryKey:['stats']})
+            queryClient.invalidateQueries({queryKey:['charts']})
+            router.push('/jobs')
+        }
+    })
+
     function onSubmit(values:CreateAndEditJobType){
-        console.log(values);
+        mutate(values)
     }
     return (
         <Form {...form}>
@@ -48,8 +72,8 @@ const CreateJobForm = () => {
                     <CustomFormSelect name='status' control={form.control} labelText='job status' items={Object.values(JobStatus)}/>
                     {/* job mode */}
                     <CustomFormSelect name='mode' control={form.control} labelText='job mode' items={Object.values(JobMode)}/>
-                    <Button type='submit' className='self-end capitalize'>
-                        create job
+                    <Button type='submit' className='self-end capitalize' disabled={isPending}>
+                        {isPending ? 'loading' : 'create job'}
                     </Button>
                 </div>
             </form>
