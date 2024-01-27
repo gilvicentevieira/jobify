80d5c6bd1826d497f5cde3265dfa486925c2af30
Gil <gilvicentevieira@gmail.com>
2024-01-27 23:12:23 +0000

    initial setup of jobcard comp

---
+++ components/JobCard.tsx
@@ -0,0 +1,12 @@
+import { JobType } from '@/utils/types'
+import React from 'react'
+
+const JobCard = ({job}:{job:JobType}) => {
+  return (
+    <div className='text-4xl'>
+      JobCard
+    </div>
+  )
+}
+
+export default JobCard
