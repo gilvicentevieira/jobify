18251a1d68a892c2c8ea818a5b8a55add8f318a8
Gil <gilvicentevieira@gmail.com>
2024-01-27 20:16:31 +0000

    Added shadcn toast component

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,10 +1,11 @@
+import CreateJobForm from '@/components/CreateJobForm'
 import React from 'react'
 
 const AddJobPage = () => {
   return (
-    <div>
-      <h1 className='text-4xl'>AddJobPage</h1>
-    </div>
+    <>
+      <CreateJobForm/>
+    </>
   )
 }
 
