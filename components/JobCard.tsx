34e5cdbe005fd48af4bf281b33529f745e428bd7
Gil <gilvicentevieira@gmail.com>
2024-01-28 04:20:28 +0000

    added jobcard comp impl

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,11 +1,39 @@
-import { JobType } from '@/utils/types'
-import React from 'react'
+import { JobType } from '@/utils/types';
+import { MapPin, Briefcase, CalendarDays, RadioTower } from 'lucide-react';
+
+import Link from 'next/link';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Separator } from './ui/separator';
+import { Button } from './ui/button';
+import { Badge } from './ui/badge';
+import JobInfo from './JobInfo';
+import DeleteJobButton from './DeleteJobBtn';
 
 const JobCard = ({job}:{job:JobType}) => {
   return (
-    <div className='text-4xl'>
-      JobCard
-    </div>
+    <Card className='bg-muted'>
+      <CardHeader>
+        <CardTitle>{job.position}</CardTitle>
+        <CardDescription>{job.company}</CardDescription>
+      </CardHeader>
+      <Separator/>
+      <CardContent>
+
+      </CardContent>
+      <CardFooter className='flex gap-4'>
+        <Button asChild size='sm'>
+          <Link href={`/jobs/${job.id}`}>edit</Link>
+        </Button>
+        <DeleteJobButton/>
+      </CardFooter>
+    </Card>
   )
 }
 
