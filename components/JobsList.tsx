5c805e4b3075eb2886ebd0b2cb7a9ed586248554
Gil <gilvicentevieira@gmail.com>
2024-01-28 04:11:30 +0000

    added jobslist impl

--- components/JobsList.tsx
+++ components/JobsList.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
+'use client';
+import JobCard from './JobCard';
+import { useSearchParams } from 'next/navigation';
+import { getAllJobsAction } from '@/utils/actions';
+import { useQuery } from '@tanstack/react-query';
 
 const JobsList = () => {
+  const searchParams = useSearchParams()
+  const search = searchParams.get('search') || ''
+  const jobStatus = searchParams.get('jobStatus') || 'all'
+
+  const pageNumber = Number(searchParams.get('page')) || 1
+
+  const {data, isPending} = useQuery({
+    queryKey: ['jobs', search, jobStatus, pageNumber],
+    queryFn:()=>getAllJobsAction({search, jobStatus, page: pageNumber})
+  })
+
+  const jobs = data?.jobs || []
+
+  if(isPending) return <h2 className='text-xl'>Please wait....</h2>
+  if(jobs.length < 1) return <h2 className='text-xl'>No Jobs Found....</h2>
+
   return (
-    <div className='text-4xl'>
-      JobsList
-    </div>
+    <>
+      {/* button container */}
+      <div className='grid md:grid-cols-2 gap-2'>
+        {jobs.map((job)=>(
+          <JobCard key={job.id} job={job}/>
+        ))}
+      </div>
+    </>
   )
 }
 
